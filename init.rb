2d9f1cbca041f570a79ad8df5a3a4fd6f5a461f3
Tim Harper <timcharper@gmail.com>
2010-04-21 03:29:33 +0800

    adds mongo_mapper as dependency for plugin

--- init.rb
+++ init.rb
@@ -1,4 +1,6 @@
 # Include hook code here
+require "mongo_mapper"
+
 require File.join(File.dirname(__FILE__), "lib", "mongration")
 require File.join(File.dirname(__FILE__), "lib", "mongo_mapper", "mongration")
 require File.join(File.dirname(__FILE__), "lib", "mongo_mapper", "migration_proxy")
